33d3254f39b203e2e8efc1c884ad7bd51578482b
Nick Moline <nick@storyllp.com>
2026-03-06 00:04:15 -0700

    handling environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import process from 'process'
+
 
 export default defineConfig({
   base: '/',
+  host: process.env.VITE_HOST,
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5175,
+    host: '0.0.0.0',
+    allowedHosts: [
+      process.env.VITE_HOST
+    ]
+  }
 })
